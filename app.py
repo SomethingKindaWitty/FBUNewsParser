ac48d10433f47f261bf04a507c685b50eef6b602
Gavin Lifrieri <gavinlifrieri@fb.com>
2018-07-17 11:38:58 -0700

    updated the api to get keywords

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 from flask import Flask, json, request, jsonify
 from newspaper import Article
 import newspaper
+import nltk
 app = Flask(__name__)
-
-@app.route("/getarticle", methods=["POST"])
+nltk.download('punkt')
+@app.route("/getArticle", methods=["POST"])
 def hello():
     data = request.json
     url = data["url"]
     article = Article(url)
     article.download()
     article.parse()
+    article.nlp()
     returnData = {
-        "text": article.text
+        "text": article.text,
+        "keywords": article.keywords
     }
     # cnn_paper = newspaper.build('http://cnn.com')
     # for article in cnn_paper.articles:
