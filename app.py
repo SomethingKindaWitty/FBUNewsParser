a1370ea55ed8530223947c6aa8cc2994c8de3ddc
Gavin Lifrieri <gavinlifrieri@fb.com>
2018-07-18 15:32:54 -0700

    added test route to see if google sentiment analysis will work on heroku

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from flask import Flask, json, request, jsonify
 from newspaper import Article
 import newspaper
 import nltk
+
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+
 app = Flask(__name__)
 nltk.download('punkt')
 @app.route("/getArticle", methods=["POST"])
@@ -23,3 +28,23 @@ def hello():
     #     print(article.url)
 
     return jsonify(returnData)
+
+@app.route("/test")
+def test():
+    # Imports the Google Cloud client library
+
+    # Instantiates a client
+    client = language.LanguageServiceClient()
+
+    # The text to analyze
+    text = u'Hello, world!'
+    document = types.Document(
+        content=text,
+        type=enums.Document.Type.PLAIN_TEXT)
+
+    # Detects the sentiment of the text
+    sentiment = client.analyze_sentiment(document=document).document_sentiment
+
+    print('Text: {}'.format(text))
+    print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
+    return 'Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude)
